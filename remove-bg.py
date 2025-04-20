dda42499622a0e64dd92bf2d223a8b0ee4f9e38c
Unknown Guy <91245369+unknownguy49@users.noreply.github.com>
2025-04-21 03:37:09 +0530

    Update remove-bg.py

--- remove-bg.py
+++ remove-bg.py
@@ -1,6 +1,4 @@
 import os
-port = os.environ.get("PORT", 8000)
-
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
@@ -22,3 +20,8 @@ async def remove_bg(file: UploadFile = File(...)):
     contents = await file.read()
     result = remove(contents)
     return StreamingResponse(BytesIO(result), media_type="image/png")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Fallback to 8000 for local testing
+    uvicorn.run(app, host="0.0.0.0", port=port)
